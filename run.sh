44bb212ee92ee4cd78c2a8983d35f41f043a7530
Sylvain Faucherand <syl.faucherand@gmail.com>
2012-11-06 15:28:47 +0100

    simplify mongo db server launch

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "Launch mongod on in new shell (new terminal window)"
       osascript -e 'tell app "Terminal"
-        do script "/usr/local/Cellar/mongodb/2.2.0-x86_64/bin/mongod"
+        do script "mongod"
       end tell' & node app
